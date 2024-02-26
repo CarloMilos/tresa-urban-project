cc62a4aafd462031a76394dd5ff2575f6978ce6d
Joe <111279157+joekts@users.noreply.github.com>
2024-02-26 13:50:55 +0000

    4 hours

--- map.php
+++ map.php
@@ -1,20 +1,37 @@
+<?php
+    // Include the config file
+    require 'config.php';
+
+    //Get TRESA area coordinates from the config file and turn into JSON array
+    $tresaCoords = json_encode($tresaCoords);
+
+    //Get public green space areas coordinates from the config file and turn into JSON array
+    $areas = json_encode($areas);
+?>
+
 <!DOCTYPE html>
 <html>
-<head>
     <title>Place and Remove Public Green Space Marker</title>
+    <link rel="stylesheet" href="styles.css">
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyATR9HPYozaZE1YdlI1b7Fn_k34TtRXzLg&libraries=geometry"></script>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/gh/geocodezip/geoxml3@master/polys/geoxml3.js"></script>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/gh/geocodezip/geoxml3@master/ProjectedOverlay.js"></script>
     <script>
         var map;
         var markers = [];
+        var polygons = [];
 
-        var myParser = new geoXML3.parser({map: map});
-        myParser.parse('tresa.kml');
+        // Initialise coordinates from config file as JS variables
+        var tresaCoords = <?php echo $tresaCoords; ?>;
+        var areas = <?php echo $areas; ?>;
 
+        // Create infoWindow variable so that only one infoWindow is open at a time
+        var infoWindow = new google.maps.InfoWindow();
+
+        // Initialise TRESA area map
         function initMap() {
+
+            // Create a new map, center is co-ords in the middle of the TRESA area
             map = new google.maps.Map(document.getElementById('map'), {
-                center: {lat: 51.454514, lng: -2.587910},
+                center: {lat: 51.44148754688007, lng: -2.576942891944935},
                 zoom: 15
             });
 
@@ -22,10 +39,48 @@
                 placeMarker(event.latLng);
             });
 
-            var myParser = new geoXML3.parser({map: map});
-            myParser.parse('tresa.kml');
+            // Draw TRESA area on the map
+            tresaArea = new google.maps.Polyline({
+                path: tresaCoords,
+                geodesic: true,
+                strokeColor: '#073b38',
+                strokeOpacity: 0.8,
+                strokeWeight: 2
+            });
 
+            // Set TRESA area on the map
+            tresaArea.setMap(map);
+
+            
+            // Loops through the areas array and draws the polygons on the map
+            for(let i = 0; i<Object.keys(areas).length; i++){
+                
+                //Draws polygon i on the map
+                polygons[i] = new google.maps.Polygon({
+                    paths: areas[i],
+                    strokeColor: '#00ff44',
+                    strokeOpacity: 0.8,
+                    strokeWeight: 2,
+                    fillColor: '#4ff77c',
+                    fillOpacity: 0.35
+                });
+
+                // Set polygon i on the map
+                polygons[i].setMap(map);
+                
+                // Add click event listener to the polygon, so that when clicked, an infoWindow opens
+                google.maps.event.addListener(polygons[i], 'click', function(event) {
+                        var size = google.maps.geometry.spherical.computeArea(polygons[i].getPath());
+                        var contentString = 'You clicked on the polygon at ' + event.latLng + '<br>Area: ' + size + ' m<sup>2</sup>';
+                        infoWindow.setContent(contentString);
+                        infoWindow.setPosition(event.latLng);
+                        infoWindow.open(map);
+                });
+            }
+            
         }
+
+        
 
         function placeMarker(location) {
             var marker = new google.maps.Marker({
@@ -69,10 +124,14 @@
             document.getElementById('removeButton').style.display = 'none'; // Hide the remove button
             document.getElementById('markerDetailsForm').style.display = 'none'; // Hide the marker details form
         }
+
+        
     </script>
 </head>
 <body onload="initMap()">
-    <div id="map" style="height: 400px; width: 100%;"></div>
+    <div id="map" style="height: 400px; width: 50%;"></div>
+    
+
     <form id="markerDetailsForm" action="handle_marker.php" method="post" style="display: none;">
         <input type="hidden" id="latitude" name="latitude">
         <input type="hidden" id="longitude" name="longitude">
